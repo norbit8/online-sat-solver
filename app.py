5a0ccabe203abfa03d0bcf8da46d5b730208e8f3
norbit8 <norbit8@gmail.com>
2021-01-29 13:42:45 +0200

    fixed

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def homepage():
     """.format(the_cnf=cnf)
 
 @app.route('/yoav')
-def homepage():
+def bruh():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
     cnf, variables, phi = parse('(p3->p2)')
     return send_from_directory('/templates', 'index.html')
