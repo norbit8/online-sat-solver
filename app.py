36a4e7c0f12cb822ac771ad1b16cfb1f4c68579e
norbit8 <norbit8@gmail.com>
2021-01-29 13:40:48 +0200

    lmao

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from datetime import datetime
 from Parser import *
 
@@ -7,11 +7,17 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-    cnf, variables, phi = parse('(p3->p2)');	
+    cnf, variables, phi = parse('(p3->p2)')
     return """
     <h1>Hello heroku</h1>
     <p>(p3->p2) in cnf is {the_cnf}.</p>
     """.format(the_cnf=cnf)
+
+@app.route('/yoav')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    cnf, variables, phi = parse('(p3->p2)')
+    return send_from_directory('/templates', 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
